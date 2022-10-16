17370b4967c5f2cc66b6927706dde40e49912d94
uRufus <71590396+uRufus@users.noreply.github.com>
2022-10-16 15:29:35 +0300

    Homework 7

--- leroy_merlin/leroy_merlin/pipelines.py
+++ leroy_merlin/leroy_merlin/pipelines.py
@@ -8,10 +8,16 @@
 from itemadapter import ItemAdapter
 from scrapy import Request
 from scrapy.pipelines.images import ImagesPipeline
-
+from pymongo import MongoClient
 
 class LeroyMerlinPipeline:
+    def __init__(self):
+        client = MongoClient('mongodb://127.0.0.1:27017/')
+        self.mongo_db = client.goods
+
     def process_item(self, item, spider):
+        collection = self.mongo_db[spider.name]
+        collection.insert_one(item)
         return item
 
 
