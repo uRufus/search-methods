d107e1f9217a119def9830d2e8b1fba0e89dc098
uRufus <71590396+uRufus@users.noreply.github.com>
2022-10-18 22:18:11 +0300

    Homework 8

---
+++ instagram/instagram/items.py
@@ -0,0 +1,15 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstagramItem(scrapy.Item):
+    # define the fields for your item here like:
+    user_id = scrapy.Field()
+    username = scrapy.Field()
+    photo = scrapy.Field()
+    likes = scrapy.Field()
+    post_data = scrapy.Field()
